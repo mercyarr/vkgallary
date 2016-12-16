b9c46e2c3db86e540417407fdf5ae407c83759c7
mercyarr <gbm1989@gmail.com>
2016-12-16 19:38:30 +0200

    init commit

---
+++ app/src/main/java/com/mercyarr/gbm/vkgallry/App.java
@@ -0,0 +1,8 @@
+package com.mercyarr.gbm.vkgallry;
+
+/**
+ * Created by gbm19 on 16.12.2016.
+ */
+
+public class App {
+}
--- app/src/main/java/com/mercyarr/gbm/vkgallry/features/main/MainActivity.java
+++ app/src/main/java/com/mercyarr/gbm/vkgallry/features/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.mercyarr.gbm.vkgallry;
+package com.mercyarr.gbm.vkgallry.features.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.mercyarr.gbm.vkgallry.R;
 
 public class MainActivity extends AppCompatActivity {
 
