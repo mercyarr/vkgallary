6287acc74d0d31ac9fdbe5267ae80169b8cda7e9
mercyarr <gbm1989@gmail.com>
2016-12-16 19:48:56 +0200

    init commit

--- app/src/main/java/com/mercyarr/gbm/vkgallry/App.java
+++ app/src/main/java/com/mercyarr/gbm/vkgallry/App.java
@@ -1,8 +1,21 @@
 package com.mercyarr.gbm.vkgallry;
 
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import io.realm.Realm;
 /**
  * Created by gbm19 on 16.12.2016.
  */
 
-public class App {
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Fresco.initialize(this);
+        Realm.init(this);
+    }
 }
